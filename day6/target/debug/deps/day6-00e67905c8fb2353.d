09f4a2ba14bd44f377eb7d48658be6c42b5b5b7e
Emil Ekberg <e_man604@hotmail.com>
2019-12-06 19:02:51 +0100

    cleanup

--- day6/target/debug/deps/day6-00e67905c8fb2353.d
+++
@@ -1,7 +0,0 @@
-/Users/emilekberg/Development/rust/adventofcode_2019/day6/target/debug/deps/day6-00e67905c8fb2353: src/main.rs src/body.rs src/orbit_map.rs
-
-/Users/emilekberg/Development/rust/adventofcode_2019/day6/target/debug/deps/day6-00e67905c8fb2353.d: src/main.rs src/body.rs src/orbit_map.rs
-
-src/main.rs:
-src/body.rs:
-src/orbit_map.rs:
--- day6/target/debug/deps/day6-324098e130fa5be0.d
+++
@@ -1,7 +0,0 @@
-/Users/emilekberg/Development/rust/adventofcode_2019/day6/target/debug/deps/day6-324098e130fa5be0: src/main.rs src/body.rs src/orbit_map.rs
-
-/Users/emilekberg/Development/rust/adventofcode_2019/day6/target/debug/deps/day6-324098e130fa5be0.d: src/main.rs src/body.rs src/orbit_map.rs
-
-src/main.rs:
-src/body.rs:
-src/orbit_map.rs:
