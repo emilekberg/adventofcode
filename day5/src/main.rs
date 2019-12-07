1d3f0042e5d3937c337874cd13688981c6d1fddf
Emil Ekberg <ekberg.emil@gmail.com>
2019-12-07 11:19:59 +0100

    rename intcode

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,12 @@
 extern crate intcode;
 
 fn main() {
-  let memory = std::fs::read_to_string("./input.txt")
+  let mut memory: Vec<i32> = std::fs::read_to_string("./input.txt")
     .unwrap()
     .split(",")
     .map(|x| x.parse().unwrap())
-    .collect(); 
-  let (res, output, _) = intcode::run_program(memory);
+    .collect();
+  let (res, output, _) = intcode::run_program(&mut memory);
   println!("first position: {}", res);
   println!("output last: {}", output.last().unwrap());
 }
