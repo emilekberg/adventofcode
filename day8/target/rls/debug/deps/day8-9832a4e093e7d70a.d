2ab65c528b894ad4937043852737afb8a38252aa
Emil Ekberg <ekberg.emil@gmail.com>
2019-12-08 14:58:05 +0100

    solved day8

--- day8/target/rls/debug/deps/day8-9832a4e093e7d70a.d
+++
@@ -1,5 +0,0 @@
-/mnt/e/dev/adventofcode_2019/day8/target/rls/debug/deps/day8-9832a4e093e7d70a.rmeta: src/main.rs
-
-/mnt/e/dev/adventofcode_2019/day8/target/rls/debug/deps/day8-9832a4e093e7d70a.d: src/main.rs
-
-src/main.rs:
