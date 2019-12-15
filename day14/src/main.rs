77aaf7a21b225959524f34898bfc742d7d286617
Emil Ekberg <ekberg.emil@gmail.com>
2019-12-15 22:58:44 +0100

    day14 part1 solved

---
+++ day14/src/main.rs
@@ -0,0 +1,122 @@
+use std::collections::HashMap;
+mod nano_factory;
+extern crate regex;
+use regex::Regex;
+fn main() {
+  let content: String = std::fs::read_to_string("./test_03.txt")
+    .unwrap();
+
+  let map = get_map_from_string(content);
+  part1(&map);
+  part2(&map);
+  
+}
+
+fn part1(map: &HashMap<(i64, String), Vec<(i64, String)>>) {
+  let mut factory = nano_factory::new(map.clone());
+  factory.produce(String::from("FUEL"), 1);
+  println!("Part1: {}", factory.required_ore);
+}
+
+fn part2(map: &HashMap<(i64, String), Vec<(i64, String)>>) {
+  println!("part 2");
+  for i in 0..10 {
+    let mut factory = nano_factory::new(map.clone());
+    factory.produce(String::from("FUEL"), i * 100);
+    println!("{}", factory.required_ore);
+  }
+
+}
+
+fn get_map_from_string(s: String) -> HashMap<(i64, String), Vec<(i64, String)>> {
+  let content: Vec<(Vec<String>)> = s
+    .lines()
+    .map(|x| x.to_string())
+    .map(|x| x
+      .split(" => ")
+      .map(|y| y.to_string()).collect()
+    )
+    .collect();
+  
+  let mut map: HashMap<(i64, String), Vec<(i64, String)>> = HashMap::new();
+
+  for to_parse in content.iter() {
+    // ["10 a"],["20 b"] => map.insert("20b", "10a");
+    
+    // key
+    let key = string_to_material_info(to_parse[1].clone()); 
+    // value
+    let mut value: Vec<(i64, String)> = vec![];
+    
+    to_parse[0].trim().split(",").for_each(|x| {
+      let val = string_to_material_info(String::from(x));
+      value.push(val);
+    });
+
+    map.insert(key, value);
+  }
+  return map;
+}
+
+fn string_to_material_info(s: String) -> (i64, String) {
+  let key_re = Regex::new(r"(\d+) (\w+)").unwrap();
+  let mut info: (i64, String) = (0, String::default());
+  for cap in key_re.captures_iter(&s) {
+    let amount: i64 = cap[1].parse().unwrap();
+    let mat: String = String::from(&cap[2]);
+    info = (amount, mat);
+  }
+  return info;
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_first() {
+    let map = get_map_from_string(std::fs::read_to_string("./test_01.txt").unwrap());
+    println!("{:?}", map);
+    let mut factory = nano_factory::new(map);
+    factory.produce(String::from("FUEL"), 1);
+
+    assert_eq!(factory.required_ore, 31);
+  }
+  #[test]
+  fn test_second() {
+    let map = get_map_from_string(std::fs::read_to_string("./test_02.txt").unwrap());
+    println!("{:?}", map);
+    let mut factory = nano_factory::new(map);
+    factory.produce(String::from("FUEL"), 1);
+
+    assert_eq!(factory.required_ore, 165);
+  }
+
+  #[test]
+  fn test_third() {
+    let map = get_map_from_string(std::fs::read_to_string("./test_03.txt").unwrap());
+    println!("{:?}", map);
+    let mut factory = nano_factory::new(map);
+    factory.produce(String::from("FUEL"), 1);
+
+    assert_eq!(factory.required_ore, 13312);
+  }  
+  #[test]
+  fn test_fourth() {
+    let map = get_map_from_string(std::fs::read_to_string("./test_04.txt").unwrap());
+    println!("{:?}", map);
+    let mut factory = nano_factory::new(map);
+    factory.produce(String::from("FUEL"), 1);
+
+    assert_eq!(factory.required_ore, 180697);
+  }
+  #[test]
+  fn test_fifth() {
+    let map = get_map_from_string(std::fs::read_to_string("./test_05.txt").unwrap());
+    println!("{:?}", map);
+    let mut factory = nano_factory::new(map);
+    factory.produce(String::from("FUEL"), 1);
+
+    assert_eq!(factory.required_ore, 2210736);
+  }  
+}
---
+++ day14/src/nano_factory.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+#[derive(Debug)]
+pub struct Material {
+  pub requires: Vec<(i64, String)>,
+  pub produces: i64,
+}
+fn new_material(requires: Vec<(i64, String)>, produces: i64) -> Material {
+  return Material {
+    requires,
+    produces,
+  };
+}
+
+#[derive(Debug)]
+pub struct NanoFactory {
+  pub available_materials: HashMap<String, i64>,
+  pub reactions: HashMap<String, (Material)>,
+  pub required_ore: i64,
+}
+pub fn new(reactions_dictionary: HashMap<(i64, String), Vec<(i64, String)>>) -> NanoFactory {
+  let mut factory = NanoFactory {
+    reactions: HashMap::new(),
+    available_materials: HashMap::new(),
+    required_ore: 0,
+  };
+  for (key, value) in reactions_dictionary.iter() {
+    let requires = value.iter().cloned().map(|(x,y)| (x, String::from(y))).collect();
+    let produces = (key.0, key.1.clone());
+    let mat = new_material(requires, produces.0);
+    factory.reactions.insert(produces.1, mat);
+  }
+  factory.reactions.insert(String::from("ORE"), new_material(vec![], 1));
+  // make materials
+  return factory;
+}
+
+impl NanoFactory {
+  fn require_ore(&mut self, amount: i64) {
+    self.required_ore += amount;
+    let ore = String::from("ORE");
+    self.available_materials.entry(ore).or_insert(amount);
+  }
+  fn get_available_materials(&mut self, material: String) -> i64 {
+    let entry = self.available_materials.entry(material).or_insert(0);
+    if *entry > 0 {
+      let ret = entry.clone();
+      *entry = 0;
+      return ret;
+    }
+    return 0;
+  }
+  pub fn produce(&mut self, material: String, amount: i64) {
+    let mut to_produce = amount - self.get_available_materials(material.clone());
+    if material == "ORE" {
+      self.require_ore(to_produce);
+      return;
+    }
+    loop {
+      if to_produce <= 0 {
+        break;
+      }
+      for i in 0..self.reactions[&material].requires.len() {
+        self.produce(self.reactions[&material].requires[i].1.clone(), self.reactions[&material].requires[i].0);
+      }
+      
+      to_produce -= self.reactions[&material].produces;
+    }
+    if to_produce < 0 {
+      *self.available_materials.entry(material).or_insert(0) += 0-to_produce;
+    }
+  }
+}
