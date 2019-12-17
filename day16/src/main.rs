a401bd8e2c17143f069f6fe72133f40c51130eca
Emil Ekberg <ekberg.emil@gmail.com>
2019-12-17 22:44:31 +0100

    day16 cleanup...

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,21 +1,20 @@
 fn main() {
 
-    let input: String = std::fs::read_to_string("./input.txt")
-        .unwrap();
+    // let input: String = std::fs::read_to_string("./input.txt").unwrap();
     
-
+    let input = String::from("03036732577212944063491565474664");
     part1(input.clone()); 
-    part2(input.clone());
+    // part2(input.clone());
 }
 
-fn str_to_int_array(s: String) -> Vec<i64> {
+fn str_to_int_array(s: String) -> Vec<i32> {
     return s
         .chars()
-        .map(|c| c.to_digit(10).unwrap() as i64)
+        .map(|c| c.to_digit(10).unwrap() as i32)
         .collect()
 }
 
-fn part1(s: String) -> i64 {
+fn part1(s: String) -> i32 {
     let input = str_to_int_array(s);
     let base_pattern = vec![0,1,0,-1];
     let result = fft_passes(&input, &base_pattern, 100);
@@ -25,20 +24,20 @@ fn part1(s: String) -> i64 {
     return answer;
 }
 
-fn part2(s: String) -> i64 {
+fn part2(s: String) -> i32 {
     let input = str_to_int_array(s);
     let base_pattern = vec![0,1,0,-1];
     let extended_input = extend_array(&input, 10000);
     println!("extended input from {} to {}", input.len(), extended_input.len());
     let result = fft_passes(&extended_input, &base_pattern, 100);
-    let message_offset = array_to_digit(result[0..7].to_vec()) as usize;
+    let message_offset = array_to_digit(input[0..7].to_vec()) as usize;
     println!("message offset {:?}, {}", message_offset, result.len());
     let answer = array_to_digit(result[message_offset..message_offset+8].to_vec());
     println!("part2: {}", answer);
     return answer;
 }
 
-fn fft_passes(input: &Vec<i64>, base_pattern: &Vec<i64>, passes: i32) -> Vec<i64> {
+fn fft_passes(input: &Vec<i32>, base_pattern: &Vec<i32>, passes: i32) -> Vec<i32> {
     let mut result = input.clone();
     for i in 0..passes {
         result = fft(&result, &base_pattern);
@@ -48,73 +47,73 @@ fn fft_passes(input: &Vec<i64>, base_pattern: &Vec<i64>, passes: i32) -> Vec<i64
 }
 
 #[inline(always)]
-fn fft(input: &Vec<i64>, pattern: &Vec<i64>) -> Vec<i64> {
-    let mut to_return = vec![0;input.len()];
-    for i in 0..input.len() {
-        let result = multiply(&input, &pattern, i);
-        to_return[i] = drop_digits(result);
-    }
-    return to_return;
+fn fft(input: &Vec<i32>, pattern: &Vec<i32>) -> Vec<i32> {
+    return input.iter().enumerate().map(|(i,_)| {
+        println!("");
+        return drop_digits(multiply(&input, &pattern, i));
+    }).collect();
 }
 
+fn fft_2(input: &Vec<i32>, pattern: &Vec<i32>) -> Vec<i32> {
+    
+    
+    let mut ret = vec![0i32;input.len()];
+    ret[input.len()-1] = input[input.len()-1];
 
+    let len = input.len()-2;
+    for i in 0..len {
+        ret[len-i] = (input[len-i] + ret[len-i+1]) % 10;
+    }
+    return ret;
+    /* 
+    for i in 0..input.len() {
+        let mut result = 0;
+        for j in i..input.len()/2 {
+            let pattern_index = ((1+j)/(i+1))%pattern.len();
+            let multiply_by = pattern[pattern_index];
+            result += input[j] * multiply_by;
+        }
+        
+        ret[i] = result % 10;
+    }
+    */
+    return ret;
+}
 
 #[inline(always)]
-fn multiply(input: &Vec<i64>, pattern: &Vec<i64>, pattern_offset: usize) -> i64 {
-    let mut result = 0;
-    return input.iter().enumerate().skip(pattern_offset).fold(0, move |acc, (i,_)| {
+fn multiply(input: &Vec<i32>, pattern: &Vec<i32>, pattern_offset: usize) -> i32 {
+    return input.iter().enumerate().skip(pattern_offset).fold(0, move |acc, (i,val)| {
         let index = ((1+i)/(pattern_offset+1))%pattern.len();
         let multiply_by = pattern[index];
+        print!("{},", multiply_by);
         if multiply_by == 0 {
             return acc;
         }
-        return acc + input[i] * multiply_by;
+        return acc + (val * multiply_by);
     });
-    /*
-    for i in pattern_offset..input.len() {
-        let index = ((1+i)/(pattern_offset+1))%pattern.len();
-        let multiply_by = pattern[index];
-        if multiply_by == 0 {
-            continue;
-        }
-        result += input[i] * multiply_by;
-    }
-    */
-    return result;
 }
 
 #[inline(always)]
-fn duplicate_pattern(pattern: &Vec<i64>, times: usize) -> Vec<i64> {
-    let mut result = vec![];
-    for p in pattern.iter() {
-        for _ in 0..times {
-            result.push(p.clone());
-        }
-    }
-    return result;
-}
-
-#[inline(always)]
-fn drop_digits(num: i64) -> i64 {
+fn drop_digits(num: i32) -> i32 {
     return (num % 10).abs();
 }
 
-fn get_num_digits_in_integer(digit: i64) -> i64 {
+fn get_num_digits_in_integer(digit: i32) -> i32 {
     if digit > 0 {
-        return ((digit as f32).log(10.) + 1.) as i64;
+        return ((digit as f32).log(10.) + 1.) as i32;
     }
     return 1;
 }
 
 #[inline(always)]
-pub fn get_nth(num: i64, n: u32) -> i64 {
-    let div = (10u32).pow(n) as i64;
+pub fn get_nth(num: i32, n: u32) -> i32 {
+    let div = (10u32).pow(n) as i32;
     let result = (num / div) % 10;
     return result;
 }
 
 #[inline(always)]
-pub fn digit_to_array(digit: i64) -> Vec<i64> {
+pub fn digit_to_array(digit: i32) -> Vec<i32> {
     let n = get_num_digits_in_integer(digit);
     let mut result = vec![];
     for i in 0..n {
@@ -125,7 +124,7 @@ pub fn digit_to_array(digit: i64) -> Vec<i64> {
 }
 
 #[inline(always)]
-fn array_to_digit(array: Vec<i64>) -> i64 {
+fn array_to_digit(array: Vec<i32>) -> i32 {
     let mut result = 0;
     let mut mul = 1;
     for (_,val) in array.iter().rev().enumerate() {
@@ -136,7 +135,7 @@ fn array_to_digit(array: Vec<i64>) -> i64 {
 }
 
 #[inline(always)]
-fn extend_array(array: &Vec<i64>, num: i32) -> Vec<i64> {
+fn extend_array(array: &Vec<i32>, num: i32) -> Vec<i32> {
     let mut result = vec![];
     for _ in 0..num {
         result.extend(array);
@@ -158,13 +157,6 @@ mod tests {
     fn test_drop_digits() {
         assert_eq!(drop_digits(38), 8);
         assert_eq!(drop_digits(-17), 7);
-    }
-    #[test]
-    fn test_duplicate_pattern() {
-        let pattern = vec![0,1,2,3];
-        assert_eq!(duplicate_pattern(&pattern, 1), vec![0,1,2,3]);
-        assert_eq!(duplicate_pattern(&pattern, 2), vec![0,0,1,1,2,2,3,3]);
-        assert_eq!(duplicate_pattern(&pattern, 3), vec![0,0,0,1,1,1,2,2,2,3,3,3]);
     }
 
     #[test]
