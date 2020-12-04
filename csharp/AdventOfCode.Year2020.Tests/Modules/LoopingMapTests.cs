56fe10cfa6f7691b5771a6ee2724905cd510c66f
Emil Ekberg <e_man604@hotmail.com>
2020-12-04 22:04:27 +0100

    day3 implemented

---
+++ csharp/AdventOfCode.Year2020.Tests/Modules/LoopingMapTests.cs
@@ -0,0 +1,37 @@
+
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Year2020.Modules;
+
+namespace AdventOfCode.Year2020.Tests.Modules
+{
+  public class LoopingMapTests
+  {
+
+    [Theory]
+    [InlineData(0,0, PositionInformation.Empty)]
+    [InlineData(2,0, PositionInformation.Tree)]
+    [InlineData(2,2, PositionInformation.Tree)]
+    // tests position loops
+    [InlineData(12,0, PositionInformation.Empty)]
+    
+    public void GetPosition_ReturnsCorrectInformation(int x, int y, PositionInformation expected)
+    {
+      var input = @"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+      var map = new LoopingMap(input);
+      var actual = map.GetPosition(x, y);
+      Assert.Equal(expected, actual);
+    }
+  }
+}
---
+++ csharp/AdventOfCode.Year2020/Modules/LoopingMap.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace AdventOfCode.Year2020.Modules
+{
+  public enum PositionInformation
+  {
+    Empty,
+    Tree
+  }
+  public class LoopingMap
+  {
+    List<List<string>> _map;
+    public int Height {get; set;}
+    public int Width {get; set;}
+    public LoopingMap(string input)
+    {
+      _map = new List<List<string>>();
+      var width = input.IndexOf(Environment.NewLine);
+
+      var currentRow = 0;
+      input.ToCharArray()
+        .Select(x => x.ToString())
+        .ToList()
+        .ForEach(x => 
+        {
+          if(x == Environment.NewLine)
+          {
+            currentRow++;
+            _map.Add(new List<string>());
+            return;
+          }
+          if(_map.Count-1 < currentRow)
+          {
+            _map.Add(new List<string>());
+          }
+          _map[currentRow].Add(x);
+        }
+      );
+      Height = _map.Count;
+      Width = _map[Height-1].Count;
+      // Console.WriteLine($"currentRow:{currentRow}");
+    }
+
+    public PositionInformation GetPosition(int x, int y)
+    {
+      if(y >= _map.Count)
+      {
+        throw new ArgumentException($"Position is not valid, y is above height x:{x},y:{y}, width:{Width},height:{Height}");
+      }
+      var row = _map[y];
+      var s = row[x % Width];
+      switch(s)
+      {
+        case ".":
+          return PositionInformation.Empty;
+        case "#":
+          return PositionInformation.Tree;
+      }
+      throw new ArgumentException($"Position is not valid x:{x},y:{y}, width:{Width},height:{Height}");
+    }
+  }
+}
