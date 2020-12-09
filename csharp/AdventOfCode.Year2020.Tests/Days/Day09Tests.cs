fab525033bd9273891446a436a7ae7fb5037f4f8
Emil Ekberg <ekberg.emil@gmail.com>
2020-12-09 20:34:46 +0100

    day 09

---
+++ csharp/AdventOfCode.Year2020.Tests/Days/Day09Tests.cs
@@ -0,0 +1,79 @@
+﻿using AdventOfCode.Year2020.Days;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.Year2020.Tests.Days
+{
+	public class Day09Tests
+	{
+		[Fact]
+		public void Part1()
+		{
+			var input = new []
+			{
+				"35",
+				"20",
+				"15",
+				"25",
+				"47",
+				"40",
+				"62",
+				"55",
+				"65",
+				"95",
+				"102",
+				"117",
+				"150",
+				"182",
+				"127",
+				"219",
+				"299",
+				"277",
+				"309",
+				"576"
+			};
+
+			var day = new Day09();
+			var actual = day.Part1(input, 5);
+			var expected = 127;
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void Part2()
+		{
+			var input = new[]
+			{
+				"35",
+				"20",
+				"15",
+				"25",
+				"47",
+				"40",
+				"62",
+				"55",
+				"65",
+				"95",
+				"102",
+				"117",
+				"150",
+				"182",
+				"127",
+				"219",
+				"299",
+				"277",
+				"309",
+				"576"
+			};
+
+			var day = new Day09();
+			var actual = day.Part2(input, 5);
+			var expected = 15 + 47; // 62
+			Assert.Equal(expected, actual);
+		}
+	}
+}
---
+++ csharp/AdventOfCode.Year2020/Days/Day09.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Common;
+namespace AdventOfCode.Year2020.Days
+{
+	public class Day09 : BaseDay<string[], long>, IDay
+	{
+		public override long Part1(string[] input) => Part1(input, 25);
+		public long Part1(string[] input, int window)
+		{
+			var data = input.Select(long.Parse).ToList();
+			int offset = 0;
+			do
+			{
+				var preamble = data.Skip(offset).Take(window).ToList();
+				var toFind = data.Skip(offset + window).Take(1).Single();
+
+				var exists = Permutations(preamble)
+					.Select(x => x.a + x.b)
+					.Any(x => x == toFind);
+				offset++;
+				if (exists) continue;
+				return toFind;
+			}
+			while (true);
+		}
+
+		public List<(long a, long b)> Permutations(List<long> data)
+		{
+			var result = new List<(long, long)>(data.Count());
+			for (int i = 0; i < data.Count(); i++)
+			{
+				for (int j = i; j < data.Count(); j++)
+				{
+					if (i == j) continue;
+					result.Add((data[i], data[j]));
+				}
+			}
+			return result;
+		}
+
+		public override long Part2(string[] input) => Part2(input, 25);
+		public long Part2(string[] input, int window)
+		{
+			var toFind = Part1(input, window);
+
+			var data = input.Select(long.Parse).ToList();
+			var start = 0;
+			var offset = 2;
+
+			do
+			{
+				var subSetOfNumbers = data.Skip(start).Take(offset);
+				var sum = subSetOfNumbers.Sum();
+				if (sum < toFind)
+				{
+					offset++;
+				}
+				else if(sum > toFind)
+				{
+					start++;
+					offset = 2;
+				}
+				else if (sum == toFind)
+				{
+					var orderedSubSetOfNumbers = subSetOfNumbers.OrderBy(x => x).ToList();
+					var low = orderedSubSetOfNumbers[0];
+					var high = orderedSubSetOfNumbers[^1];
+					return low + high;
+				}
+			}
+			while (true);
+		}
+	}
+}
