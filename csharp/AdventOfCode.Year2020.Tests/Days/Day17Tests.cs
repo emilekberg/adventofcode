e6beec1438fae6d06df74a9c434a8880ba71bdf2
Emil Ekberg <ekberg.emil@gmail.com>
2020-12-18 00:12:59 +0100

    day 17 solved

---
+++ csharp/AdventOfCode.Year2020.Tests/Days/Day17Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.Year2020.Tests.Days
+{
+	public class Day17Tests
+	{
+		[Fact]
+		public void Part1()
+		{
+			var input = new string[]
+			{
+				".#.",
+				"..#",
+				"###"
+			};
+
+			var day = new Day17();
+			var expected = 112;
+			var actual = day.Part1(input);
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void Part2()
+		{
+			var input = new string[]
+			{
+				".#.",
+				"..#",
+				"###"
+			};
+
+			var day = new Day17();
+			var expected = 848;
+			var actual = day.Part2(input);
+			Assert.Equal(expected, actual);
+		}
+	}
+}
