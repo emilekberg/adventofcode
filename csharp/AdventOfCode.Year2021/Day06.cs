f90f050ddcdf0c299314cd4c8d847b7bf904bd4d
Emil Ekberg <ekberg.emil@gmail.com>
2021-12-06 14:28:23 +0100

    cleanup day 6

--- csharp/AdventOfCode.Year2021/Day06.cs
+++ csharp/AdventOfCode.Year2021/Day06.cs
@@ -1,5 +1,4 @@
 ﻿using AdventOfCode.Common;
-using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Year2021;
 /// <Summary>
@@ -9,57 +8,31 @@ public class Day06 : BaseDay<string, long>, IDay
 {
 	public override long Part1(string input)
 	{
-		var lifetimes = input.Split(',').Select(int.Parse).OrderBy(x => x).ToList();
-		return LinkedList(lifetimes, 80);
-		// return Naive(lifetimes, 80);
+		return LinkedList(input, 80);
 	}
-
-	public long LinkedList(List<int> lifetimes, int numberOfDays)
+	public override long Part2(string input)
 	{
-		var queue = new LinkedList<long>();
-		LinkedListNode<long> sixth;
+		return LinkedList(input, 256);
+	}
+	public static long LinkedList(string input, int numberOfDays)
+	{
+		var lifetimes = input.Split(',').Select(int.Parse).ToList();
+		var list = new LinkedList<long>();
 		for(int i = 0; i <= 8; i++)
 		{
 			var count = lifetimes.Count(x => x == i);
-			queue.AddLast(count);
+			list.AddLast(count);
 		}
-		sixth = queue.Last.Previous.Previous;
+		LinkedListNode<long> sixth = list.Last.Previous.Previous;
 		for(int day = 0; day < numberOfDays; day++)
 		{
-			long fishesToAdd = queue.First.Value;
-			queue.RemoveFirst();
-			queue.AddLast(fishesToAdd);
+			long fishesToAdd = list.First.Value;
+			list.RemoveFirst();
+			list.AddLast(fishesToAdd);
 			sixth = sixth.Next;
 			sixth.ValueRef += fishesToAdd;
 		}
-		var totalNumberOfFishes = queue.Sum();
+		var totalNumberOfFishes = list.Sum();
 		return totalNumberOfFishes;
-	}
-	public long Naive(List<long> lifetimes, int numberOfDays)
-	{
-		for (int day = 0; day < numberOfDays; day++)
-		{
-			int fishesToAdd = 0;
-			for (int i = 0; i < lifetimes.Count; i++)
-			{
-				if (lifetimes[i] == 0)
-				{
-					lifetimes[i] = 6;
-					fishesToAdd++;
-				}
-				else
-				{
-					lifetimes[i]--;
-				}
-			}
-			lifetimes.AddRange(Enumerable.Repeat(8, fishesToAdd).Cast<long>());
-		}
-		return lifetimes.Count;
-	}
-
-	public override long Part2(string input)
-	{
-		var lifetimes = input.Split(',').Select(int.Parse).ToList();
-		return LinkedList(lifetimes, 256);
 	}
 }
