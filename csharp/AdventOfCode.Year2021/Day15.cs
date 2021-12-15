80d6a98ee4be52b89c389a2eee2e6c83ed3cb6b9
Emil Ekberg <ekberg.emil@gmail.com>
2021-12-15 23:55:57 +0100

    clean up day 15

--- csharp/AdventOfCode.Year2021/Day15.cs
+++ csharp/AdventOfCode.Year2021/Day15.cs
@@ -5,40 +5,33 @@ using System.Numerics;
 using System.Text;
 
 namespace AdventOfCode.Year2021;
+
+public record AStarNode(Point Position, int Cost);
 /// <Summary>
 /// https://adventofcode.com/2021/day/15
 /// </Summary> 
 public class Day15 : BaseDay<string[], long>, IDay
 {
-	public static List<AStarNode> Parse(string[] input)
+	public static Dictionary<(int x, int y), AStarNode> Parse(string[] input, int multiplier)
 	{
-		var nodes = new List<AStarNode>();
-		for (int y = 0; y < input.Length; y++)
-		{
-			for (int x = 0; x < input[y].Length; x++)
-			{
-				var node = new AStarNode(new Point(x, y), (int)char.GetNumericValue(input[y][x]));
-				nodes.Add(node);
-			}
-		}
-		return nodes;
-	}
-	public static Dictionary<(int x, int y), AStarNode> BuildGrid(List<AStarNode> nodes, int multiplier, int width, int height)
-	{
+		var baseWidth = input[0].Length;
+		var baseHeight = input.Length;
 		var grid = new Dictionary<(int x, int y), AStarNode>();
-
-		for (int row = 0; row < multiplier; row++)
+		for (int y = 0; y < baseHeight * multiplier; y++)
 		{
-			for (int col = 0; col < multiplier; col++)
+			for (int x = 0; x < baseWidth * multiplier; x++)
 			{
-				foreach (var node in nodes)
-				{
-					var cost = ((node.Cost - 1 + row + col) % 9) + 1;
-					var x = node.Position.X + width * col;
-					var y = node.Position.Y + height * row;
-					var newNode = new AStarNode(new Point(x, y), cost);
-					grid.Add((x, y), newNode); 
-				}
+				var baseCost = (int)char.GetNumericValue(input[y % baseHeight][x % baseWidth]);
+				int xCostAdd = x / baseWidth;
+				int yCostAdd = y / baseHeight;
+				// wraps around 1 and 9.
+				var cost = ((baseCost - 1 + xCostAdd + yCostAdd) % 9) + 1;
+				var xPos = x;
+				var yPos = y;
+
+
+				var node = new AStarNode(new Point(xPos, yPos), cost);
+				grid.Add((x, y), node);
 			}
 		}
 		return grid;
@@ -54,47 +47,20 @@ public class Day15 : BaseDay<string[], long>, IDay
 
 	public static long Solve(string[] input, int gridMultiplier)
 	{
-		var nodes = Parse(input);
-		var grid = BuildGrid(nodes, gridMultiplier, input[0].Length, input.Length);
+		var grid = Parse(input, gridMultiplier);
 
 		var end = grid.Values.MaxBy(x => (x.Position.X, x.Position.Y));
 		var start = grid.Values.MinBy(x => (x.Position.X, x.Position.Y));
 		var path = FindShortestPath(start, end, grid);
 
-		Print(path, grid);
-
-		return path.Skip(1).Sum(x => x.Cost);
+		return path;
 	}
-
-	public static void Print(List<AStarNode> path, Dictionary<(int x, int y), AStarNode> grid)
-	{
-		var max = grid.Values.MaxBy(x => (x.Position.X, x.Position.Y));
-		var sb = new StringBuilder();
-		sb.AppendLine();
-		for (int row = 0; row < max.Position.Y; row++)
-		{
-			sb.AppendLine();
-			for (int col = 0; col < max.Position.X; col++)
-			{
-				if (path.Find(x => x.Position.X == col && x.Position.Y == row) != null)
-				{
-					sb.Append('*');
-				}
-				else
-				{
-					sb.Append(grid[(col, row)].Cost); 
-				}
-
-			}
-		}
-		Console.WriteLine(sb.ToString());
-	}
-	public static List<AStarNode>? FindShortestPath(AStarNode start, AStarNode end, Dictionary<(int x, int y), AStarNode> nodes)
+	public static int FindShortestPath(AStarNode start, AStarNode end, Dictionary<(int x, int y), AStarNode> nodes)
 	{
 		var h = (AStarNode current, AStarNode end) => Point.ManhattanDistance(current.Position, end.Position);
 		var d = (AStarNode lhs, AStarNode rhs) => rhs.Cost;
 
-		var openSet = new PriorityQueue<AStarNode, float>();
+		var openSet = new PriorityQueue<AStarNode, int>();
 		openSet.Enqueue(start, start.Cost);
 		var openHashSet = new HashSet<AStarNode>
 		{ 
@@ -102,16 +68,18 @@ public class Day15 : BaseDay<string[], long>, IDay
 		};
 
 		var cameFrom = new Dictionary<AStarNode, AStarNode>();
-		var gScore = new DefaultDictionary<AStarNode, float>(float.MaxValue)
+		var gScore = new DefaultDictionary<AStarNode, int>(int.MaxValue)
 		{
 			[start] = 0,
 		};
-		while (openSet.TryDequeue(out var current, out var _))
+		while (openSet.TryDequeue(out var current, out var cost))
 		{
 			openHashSet.Remove(current);
 			if (current == end)
 			{
-				return ReconstructPath(cameFrom, current);
+				// for this task we only want the cost of the path to the last node.
+				// to get full path, return ReconstructPath(cameFrom, current); 
+				return cost;
 			}
 
 			foreach (var neighbour in GetNeighbours(nodes, (current.Position.X, current.Position.Y)))
@@ -132,7 +100,7 @@ public class Day15 : BaseDay<string[], long>, IDay
 			}
 		}
 
-		return null;
+		throw new Exception("Could not find path");
 	}
 
 	public static List<AStarNode> GetNeighbours(Dictionary<(int x, int y), AStarNode> nodes, (int x, int y) position)
@@ -173,4 +141,4 @@ public class Day15 : BaseDay<string[], long>, IDay
 	}
 }
 
-public record AStarNode(Point Position, int Cost);
+
